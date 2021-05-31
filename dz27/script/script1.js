7c788a4cbea7c4a18a07f707c4f6297ac48154de
unknown <15-12-1991@mail.ru>
2021-06-01 00:34:51 +0300

    Add all my files

---
+++ dz27/script/script1.js
@@ -0,0 +1,8 @@
+let ttl = document.getElementsByClassName("ttl");
+console.log(ttl[1].innerText);
+
+let price = document.getElementsByClassName("price");
+console.log(price[1].innerText);
+
+let img = document.getElementsByName('img61.jp');
+console.log(img);
